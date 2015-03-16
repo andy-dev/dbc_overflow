92e8ea7cc28d375185e1ffddb911d042df40a945
andy-dev <andres.macedo@gmail.com>
2015-03-16 16:30:35 -0700

    added some controller tests

--- dbc_overflow/spec/models/question_spec.rb
+++ dbc_overflow/spec/models/question_spec.rb
@@ -3,3 +3,20 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+
+describe QuestionsController do
+
+
+  describe "index" do
+    it "renders the index template" do
+      get :index
+      response.should contain("Listing all Questions")
+    end
+
+    it "renders the questions/index template" do
+      get :index
+      response.should contain("Listing all Questions")
+    end
+  end
+end
