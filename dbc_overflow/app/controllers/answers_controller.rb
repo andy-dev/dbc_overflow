3914adca33fcd72577961f94d437f20f5fc00866
andy-dev <andres.macedo@gmail.com>
2015-03-17 10:53:47 -0700

    edit show form to show answers of questions

--- dbc_overflow/app/controllers/answers_controller.rb
+++ dbc_overflow/app/controllers/answers_controller.rb
@@ -1,12 +1,12 @@
 class AnswersController < ApplicationController
   def create
-    @question = Question.find(params[:article_id])
+    @question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params)
     redirect_to question_path(@question)
   end
 
   private
-    def comment_params
+    def answer_params
       params.require(:answer).permit(:title, :body)
     end
 
