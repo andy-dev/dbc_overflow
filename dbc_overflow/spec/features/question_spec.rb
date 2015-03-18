6b283d8eaaee70a2fc641b6e0f5a35dea6a46379
andy-dev <andres.macedo@gmail.com>
2015-03-18 10:17:28 -0700

    corrected indentation for tests

--- dbc_overflow/spec/features/question_spec.rb
+++ dbc_overflow/spec/features/question_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 describe "add new question", :js => true do
 
-  # before :each do
-  #   Question.make(:title => 'question one', :body=> 'what the heck')
-  # end
-
-  it "create a new question" do
+  it "creates a new question" do
     visit new_question_path
-      fill_in 'Title', :with => 'question one'
-      fill_in 'Body', :with => 'what the heck'
+    fill_in 'Title', :with => 'question one'
+    fill_in 'Body', :with => 'what the heck'
 
     click_button 'Save Question'
+
     expect(page).to have_content 'question one'
+    expect(page).to have_content 'what the heck'
   end
 end
