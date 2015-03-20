8c08f3530ecb07d887ac64845b0a9ac4817d9297
andy-dev <andres.macedo@gmail.com>
2015-03-19 17:03:24 -0700

    add link_to to destroy and update answers

--- dbc_overflow/app/controllers/answers_controller.rb
+++ dbc_overflow/app/controllers/answers_controller.rb
@@ -1,13 +1,32 @@
 class AnswersController < ApplicationController
+
+  def edit
+     @answer = Answer.find(params[:id])
+  end
+
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params)
     redirect_to question_path(@question)
   end
 
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.update(answer_params)
+    redirect_to question_path(@question)
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to question_path(@question)
+  end
+
   private
     def answer_params
-      params.require(:answer).permit(:title, :body)
+      params.require(:answer).permit(:title, :body, :votes)
     end
 
 end
