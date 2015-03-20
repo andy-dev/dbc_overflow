1c4990715f37daaf1755b01744dbf69869fa8978
andy-dev <andres.macedo@gmail.com>
2015-03-19 20:37:58 -0700

    finish basic ajax for answer form needs editing

--- dbc_overflow/app/assets/javascripts/answerVotes.js
+++ dbc_overflow/app/assets/javascripts/answerVotes.js
@@ -3,10 +3,38 @@ $(function() {
 });
 
 function testbindEvents() {
+  $('.new_answer').on('submit',addAnswer);
   $('.answerupvote').on('click',answerUpVote);
   $('.answerdownvote').on('click',answerDownVote);
 }
 
+var addAnswer = function (e){
+  e.preventDefault();
+
+  var formData = $(this).serialize();
+  var url = $(this).attr("action");
+  console.log(url)
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: formData,
+    dataType: "json"
+  })
+  .done(function(answerResponse){
+    console.log("success");
+    console.log(answerResponse);
+    var html = $('#new_answer_template').html();
+    var templatingFunction = Handlebars.compile(html);
+    $('#results').append(templatingFunction({answerResponse: answerResponse}));
+    $('.answerupvote').on('click',upVote);
+    $('.answerdownvote').on('click',downVote);
+  })
+  .fail(function(){
+  console.log("fail")
+  });
+
+}
 
 var answerUpVote = function(e){
   e.preventDefault();
